d193c8e49a1becf46270e994ba195ea88c5a2f49
LuoHaoAt2017 <hluo5206@gmail.com>
2020-12-28 19:57:09 +0800

    build: json

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const pkg = require('./package.json');
 // import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
 // import typescript from '@rollup/plugin-typescript';
 
@@ -23,6 +24,7 @@ export default {
     //   exclude: ['node_modules'],
     //   extensions: ['.ts', '.js']
     // }),
+    json(),
     babel({
       babelHelpers: 'bundled',
       exclude: ['node_modules'],
