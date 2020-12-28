fc480dff2c5db36dfb89bd76d76a968f905de67d
LuoHaoAt2017 <hluo5206@gmail.com>
2020-12-28 19:18:30 +0800

    fix: commonjs -> esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,37 @@
 const path = require('path');
 const pkg = require('./package.json');
-import commonjs from '@rollup/plugin-commonjs';
+// import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+// import typescript from '@rollup/plugin-typescript';
 
 const resolve = function(...args) {
   return path.resolve(__dirname, ...args);
 };
 
+const extensions = ['.ts', '.js'];
+
 export default {
   input: resolve('./src/index.ts'),
   output: {
     file: resolve('./', pkg.main),
-    format: 'esm'
+    format: 'esm',
+    sourcemap: true,
   },
   plugins: [
-    commonjs({
-      exclude: ['node_modules'],
-      extensions: ['.ts', '.js']
-    }),
+    // commonjs({
+    //   exclude: ['node_modules'],
+    //   extensions: ['.ts', '.js']
+    // }),
     babel({
       babelHelpers: 'runtime',
       exclude: ['node_modules'],
-      extensions: ['.ts', '.js']
+      extensions
     }),
     nodeResolve({
       extensions: ['.ts', '.js']
     }),
-    typescript()
+    // typescript()
   ],
   external: [/@babel\/runtime/]
 }
